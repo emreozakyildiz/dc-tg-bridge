994da7996f7ef7d7f552ad0114c54527eeb618c6
Emre ÖZAKYILDIZ <48735717+emreozakyildiz@users.noreply.github.com>
2023-09-11 03:11:24 +0300

    Completed!

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const config = require("./config.json")
 const TelegramBot = require('node-telegram-bot-api');
+const { Client, GatewayIntentBits } = require('discord.js');
+
 
 const token = config.TELEGRAM_BOT_TOKEN;
+const discordToken = config.DISCORD_BOT_TOKEN;
+const discordChannelId = config.DISCORD_CHANNEL_ID;
 
-const bot = new TelegramBot(token, { polling: true });
+const telegramBot = new TelegramBot(token, { polling: true });
+const discordBot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const messageText = msg.text;
+telegramBot.on('message', (msg) => {
+    const messageText = msg.text;
 
-  if (msg.from.username.toString === 'DiscordMessageBridgeBot' || !messageText) {
-    return;
-  }
-  bot.sendMessage(chatId, messageText);
+    if (msg.from.username === 'DiscordMessageBridgeBot' || !messageText) {
+        return;
+    }
+    const discordChannel = discordBot.channels.cache.get(discordChannelId);
+    if (discordChannel) {
+        discordChannel.send(`[${msg.from.username}] ${messageText}`);
+    }
 });
 
-bot.on('polling_error', (error) => {
-  console.error(error);
+discordBot.on('messageCreate', (message) => {
+    if (message.channelId != config.DISCORD_CHANNEL_ID)
+        return console.log("Farklı kanal!");
+    console.log("Message Content: " + message.content)
+    if (message.author.bot || !message.content) {
+        return;
+    }
+    telegramBot.sendMessage(config.TELEGRAM_GROUP_CHAT_ID, `[${message.author.username}] ${message.content}`);
 });
+
+telegramBot.on('error', (error) => {
+    console.error(error);
+});
+
+discordBot.login(discordToken);
